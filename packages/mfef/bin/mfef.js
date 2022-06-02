1fd3af6ed089f9905870f7419aaa75b7c036b797
sonsmart <582825762@qq.com>
2022-06-02 18:18:26 +0800

    feat:modify

---
+++ packages/mfef/bin/mfef.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const {
+    program
+} = require('commander');
+
+program
+    .version(require('../package.json').version, '-v, -V', '输出当前框架的版本')
+    .description('这是21天短文，挑战手写前端框架的产物框架')
+    .usage('<command> [options]')
+    .parse(process.argv)
+
+program.command('help')
+    .alias('-h')
+    .description('帮助命令')
+    .action(function(name, other) {
+        console.log(`
+这是21天短文，挑战手写前端框架的产物框架 mfef
+
+支持的命令:
+  version, -v,-V 输出当前框架的版本
+  help,-h 输出帮助程序
+
+Example call:
+    $ mfef <command> --help`)
+    }).parse(process.argv);
